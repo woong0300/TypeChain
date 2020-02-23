e54a728e9d318695e09de26dcad2cacdd9aeb445
woong0300 <woong0300@naver.com>
2020-02-23 18:31:02 +0900

    TypeChain project is Finished

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const CryptoJS = require("crypto-js");
 class Block {
     constructor(index, hash, previousHash, data, timestamp) {
         this.index = index;
@@ -9,11 +10,56 @@ class Block {
         this.timestamp = timestamp;
     }
 }
+Block.calculateBlockHash = (index, previousHash, timestamp, data) => CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
+Block.validateStructure = (aBlock) => typeof aBlock.index === "number" &&
+    typeof aBlock.hash === "string" &&
+    typeof aBlock.previousHash === "string" &&
+    typeof aBlock.timestamp === "number" &&
+    typeof aBlock.data === "string";
 const genesisBlock = new Block(0, "20202020", "", "Hello", 123456);
-let blockchains = [genesisBlock];
-blockchains.push("Next Stuff");
+let blockchain = [genesisBlock];
+const getBlockChain = () => blockchain;
+const getLastestBlock = () => blockchain[blockchain.length - 1];
+const getNextTimeStamp = () => Math.round(new Date().getTime() / 1000);
+const createNewBlock = (data) => {
+    const previousBlock = getLastestBlock();
+    const newIndex = previousBlock.index + 1;
+    const newTimeStamp = getNextTimeStamp();
+    const newHash = Block.calculateBlockHash(newIndex, previousBlock.hash, newTimeStamp, data);
+    const newBlock = new Block(newIndex, newHash, previousBlock.hash, data, newTimeStamp);
+    addBlock(newBlock);
+    return newBlock;
+};
+const getHashForBlock = (aBlock) => Block.calculateBlockHash(aBlock.index, aBlock.previousHash, aBlock.timestamp, aBlock.data);
+const isBlockValid = (candidateBlock, previousBlock) => {
+    if (!Block.validateStructure(candidateBlock)) {
+        return false;
+    }
+    else if (previousBlock.index + 1 !== candidateBlock.index) {
+        return false;
+    }
+    else if (previousBlock.hash !== candidateBlock.previousHash) {
+        return false;
+    }
+    else if (getHashForBlock(candidateBlock) !== candidateBlock.hash) {
+        return false;
+    }
+    else {
+        return true;
+    }
+};
+const addBlock = (candidateBlock) => {
+    if (isBlockValid(candidateBlock, getLastestBlock())) {
+        blockchain.push(candidateBlock);
+    }
+};
+// blockchains.push("Next Stuff");
 //이런 실수가 발생하지 않는다. block객체가 아니여서 컴파일이 되지 않는다
 //이걸 미리 알 수 있는 것이 typescript의 장점!!
-console.log(blockchains);
+createNewBlock("Second Block");
+createNewBlock("Third Block");
+createNewBlock("Fourth Block");
+createNewBlock("Fifth Block");
+console.log(blockchain);
 // 이 파일이 모듈이 된다는 것을 알려줘야 되는 TypeScript문법
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,28 @@
+import * as CryptoJS from "crypto-js";
+
 class Block{
+    static calculateBlockHash = (
+        index:number, 
+        previousHash:string,
+        timestamp: number,
+        data:string
+    ): string => CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
+
+    static validateStructure = (aBlock:Block) : boolean => 
+        typeof aBlock.index === "number" && 
+        typeof aBlock.hash === "string" && 
+        typeof aBlock.previousHash === "string" &&
+        typeof aBlock.timestamp === "number" &&
+        typeof aBlock.data === "string";
+    
+    //static들이 위에 먼저 있고 그 아래에 나머지가 있는 구조가 일반적
+    
     public index: number;
     public hash: string;
     public previousHash: string;
     public data: string;
     public timestamp: number;
+
     constructor(
         index: number,
         hash: string,
@@ -20,15 +39,76 @@ class Block{
            
 }
 
-const genesisBlock:Block = new Block(0, "20202020", "", "Hello", 123456);
+const genesisBlock: Block = new Block(0, "20202020", "", "Hello", 123456);
 
-let blockchains: [Block] = [genesisBlock];
+let blockchain: Block[] = [genesisBlock];
 
-blockchains.push("Next Stuff");
+const getBlockChain = () : Block[] => blockchain;
+
+const getLastestBlock = (): Block => blockchain[blockchain.length - 1];
+
+const getNextTimeStamp = (): number => Math.round(new Date().getTime() / 1000);
+
+
+const createNewBlock = (data:string) : Block =>{
+    const previousBlock: Block = getLastestBlock();
+    const newIndex: number = previousBlock.index + 1;
+    const newTimeStamp : number = getNextTimeStamp();
+    const newHash: string = Block.calculateBlockHash(
+        newIndex, 
+        previousBlock.hash, 
+        newTimeStamp, 
+        data
+    );
+    const newBlock: Block = new Block(
+        newIndex, 
+        newHash, 
+        previousBlock.hash, 
+        data, 
+        newTimeStamp
+    );
+    addBlock(newBlock);
+    return newBlock;
+
+};
+
+const getHashForBlock = (aBlock: Block) :string => 
+    Block.calculateBlockHash(
+        aBlock.index,
+        aBlock.previousHash,
+        aBlock.timestamp,
+        aBlock.data
+    );
+
+const isBlockValid = (candidateBlock:Block, previousBlock:Block): boolean =>{
+    if(!Block.validateStructure(candidateBlock)){
+        return false;
+    } else if(previousBlock.index + 1 !== candidateBlock.index){
+        return false;
+    } else if(previousBlock.hash !== candidateBlock.previousHash){
+        return false;
+    } else if(getHashForBlock(candidateBlock) !== candidateBlock.hash){
+        return false;
+    } else{
+        return true;
+    }
+};
+
+const addBlock = (candidateBlock: Block) : void => {
+    if(isBlockValid(candidateBlock, getLastestBlock())){
+        blockchain.push(candidateBlock);
+    }
+};
+// blockchains.push("Next Stuff");
 //이런 실수가 발생하지 않는다. block객체가 아니여서 컴파일이 되지 않는다
 //이걸 미리 알 수 있는 것이 typescript의 장점!!
 
-console.log(blockchains);
+createNewBlock("Second Block");
+createNewBlock("Third Block");
+createNewBlock("Fourth Block");
+createNewBlock("Fifth Block");
+
+console.log(blockchain);
 
 
 //sayHi(name,age); 이거와 같이 argument 하나를 빼면 동작이 안되게 되어있어
